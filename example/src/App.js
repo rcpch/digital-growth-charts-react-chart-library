89e9c4c4d49e05ad3a93033a14916e9aaefc76fb
Simon Chapman <eatyourpeasapps@gmail.com>
2020-10-14 15:07:28 +0100

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'digital-growth-charts-react-chart-component'
+import RCPCHChartComponent from 'digital-growth-charts-react-chart-component'
 import 'digital-growth-charts-react-chart-component/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <RCPCHChartComponent 
+            measurementMethod='weight' 
+            sex='male' 
+            centilesColour='#0d0c0a' 
+            width={700} 
+            height={600}
+            minimumDecimalAge = {1}
+            maximumDecimalAge = {5.5}
+            />
 }
 
 export default App
